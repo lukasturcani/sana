e8e1201e45523f76a7b674e278d25713757c0665
Lukas Turcani <lukasturcani93@gmail.com>
2024-11-24 15:30:21 +0000

    wip

--- src/sana/_internal/scripts/app.py
+++ src/sana/_internal/scripts/app.py
@@ -2,14 +2,25 @@ import os
 
 from nicegui import native, ui
 
+from sana._internal.components.local_file_picker import LocalFilePicker
+
 
 def main() -> None:
     """Run the app."""
-    ui.label("Hello, World 0!")
     ui.run(
         port=native.find_open_port(),
         reload=os.environ.get("SANA_RELOAD", "FALSE") == "TRUE",
     )
+
+
+async def pick_file() -> None:
+    result = await LocalFilePicker("~")
+    ui.notify(f"You chose {result}")
+
+
+@ui.page("/")
+def index() -> None:
+    ui.button("Choose file", on_click=pick_file, icon="folder")
 
 
 if __name__ in {"__main__", "__mp_main__"}:
